74977f6010325dcd037b98fa46c1ed21d5fd3d6f
Pushpendra Kumar <79224572+Pushpendra6367@users.noreply.github.com>
2021-07-18 22:22:52 +0530

    Create afk.py

---
+++ alpha/plugins/afk.py
@@ -0,0 +1,157 @@
+
+
+import asyncio
+import datetime
+from datetime import datetime
+
+from telethon import events
+from telethon.tl import functions, types
+from userbot import CMD_HELP
+from userbot import ALIVE_NAME
+from darkbot.utils import admin_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "User"
+
+dark = bot.uid
+busypic = "https://telegra.ph/file/c15ceb4f540b26cd9cea5.jpg"
+
+global USER_AFK  # pylint:disable=E0602
+global afk_time  # pylint:disable=E0602
+global last_afk_message  # pylint:disable=E0602
+global afk_start
+global afk_end
+USER_AFK = {}
+afk_time = None
+last_afk_message = {}
+afk_start = {}
+
+
+@borg.on(events.NewMessage(outgoing=True))  # pylint:disable=E0602
+async def set_not_afk(event):
+    if event.fwd_from:
+        return
+    global USER_AFK  # pylint:disable=E0602
+    global afk_time  # pylint:disable=E0602
+    global last_afk_message  # pylint:disable=E0602
+    global afk_start
+    global afk_end
+    came_back = datetime.now()
+    afk_end = came_back.replace(microsecond=0)
+    if afk_start != {}:
+        total_afk_time = str((afk_end - afk_start))
+    current_message = event.message.message
+    if ".afk" not in current_message and "yes" in USER_AFK:  # pylint:disable=E0602
+        darkbot = await borg.send_message(
+            event.chat_id,
+            "__Back online!__\n**Not busy now.**\n⏱️ `Was busy for:``"
+            + total_afk_time
+            + "`", file=darkpic
+        )
+        try:
+            await borg.send_message(  # pylint:disable=E0602
+                Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+                "#AFKFALSE \nSet AFK mode to False\n"
+                + "__Back online!__\n**Not busy now.**\n⏱️ `Was busy for:``"
+                + total_afk_time
+            )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await borg.send_message(  # pylint:disable=E0602
+                event.chat_id,
+                "Please set `PRIVATE_GROUP_BOT_API_ID` "
+                + "for the proper functioning of afk functionality "
+                + "Ask in @dark_bot_support to get help setting this value\n\n `{}`".format(str(e)),
+                reply_to=event.message.id,
+                silent=True,
+            )
+        await asyncio.sleep(5)
+        await darkbot.delete()
+        USER_AFK = {}  # pylint:disable=E0602
+        afk_time = None  # pylint:disable=E0602
+
+
+@borg.on(
+    events.NewMessage(  # pylint:disable=E0602
+        incoming=True, func=lambda e: bool(e.mentioned or e.is_private)
+    )
+)
+async def on_afk(event):
+    if event.fwd_from:
+        return
+    global USER_AFK  # pylint:disable=E0602
+    global afk_time  # pylint:disable=E0602
+    global last_afk_message  # pylint:disable=E0602
+    global afk_start
+    global afk_end
+    cum_back = datetime.now()
+    afk_end = cum_back.replace(microsecond=0)
+    if afk_start != {}:
+        total_afk_time = str((afk_end - afk_start))
+    current_message_text = event.message.message.lower()
+    if "afk" in current_message_text:
+        # userbot's should not reply to other userbot's
+        # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
+        return False
+    if USER_AFK and not (await event.get_sender()).bot:
+        msg = None
+        
+        message_to_reply = ( 
+              f"**[{DEFAULTUSER}](tg://user?id={dark}) is busy now. 🙆‍♂️\n Dont disturb Please👏 **\n\n**👉Busy from** `{total_afk_time}`\n"
+            + f"\n\n**Busy in :- **\n`{reason}`"
+  if reason
+            else f"**Shhhh!**\n__I'm busy now__\n__ From __ `{total_afk_time}`\n**I'll reply you when i'll be free**"
+        )
+        msg = await event.reply(message_to_reply, file=busypic)
+        await asyncio.sleep(2)
+        if event.chat_id in last_afk_message:  # pylint:disable=E0602
+            await last_afk_message[event.chat_id].delete()  # pylint:disable=E0602
+        last_afk_message[event.chat_id] = msg  # pylint:disable=E0602
+
+
+@borg.on(admin_cmd(pattern=r"busy(.*)", outgoing=True))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    harsh = await event.get_reply_message()
+    global USER_AFK  # pylint:disable=E0602
+    global afk_time  # pylint:disable=E0602
+    global last_afk_message  # pylint:disable=E0602
+    global afk_start
+    global afk_end
+    global reason
+    global darkpic
+    USER_AFK = {}
+    afk_time = None
+    last_afk_message = {}
+    afk_end = {}
+    start_1 = datetime.now()
+    afk_start = start_1.replace(microsecond=0)
+    reason = event.pattern_match.group(1)
+    darkpic = await event.client.download_media(harsh) 
+    if not USER_AFK:  # pylint:disable=E0602
+        last_seen_status = await borg(  # pylint:disable=E0602
+            functions.account.GetPrivacyRequest(types.InputPrivacyKeyStatusTimestamp())
+        )
+        if isinstance(last_seen_status.rules, types.PrivacyValueAllowAll):
+            afk_time = datetime.datetime.now()  # pylint:disable=E0602
+        USER_AFK = f"yes: {reason} {darkpic}"  # pylint:disable=E0602
+        if reason:
+            await borg.send_message(
+                event.chat_id, f"__**I'm busy now**__ \n⚜ Because `{reason}`", file=darkpic
+            )
+        else:
+            await borg.send_message(event.chat_id, f"**I am Busy now!**�", file=darkpic)
+        await asyncio.sleep(0.001)
+        await event.delete()
+        try:
+            await borg.send_message(  # pylint:disable=E0602
+                Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+                f"#AFKTRUE \nSet AFK mode to True, and Reason is {reason}",file=darkpic
+            )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            logger.warn(str(e))  # pylint:disable=E0602
+
+
+CmdHelp("afk").add_command(
+  'busy', '<reply to media>/<or type a reson>', 'Marks you offline with reason(if given) also shows offline time. Media also supported.'
+).add()
